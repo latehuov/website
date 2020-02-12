8c99ef875d2474a52b6c5528bb116e8ad8b48745
lasse <t8hula00@students.oamk.fi>
2020-02-12 22:12:01 +0200

    pers

--- js/pagefunctionality.js
+++ js/pagefunctionality.js
@@ -1,23 +1,21 @@
 function loadDoc(page) {
-  var xhr = new XMLHttpRequest();
-  xhr.open("GET", page, true);
-  xhr.onreadystatechange = function() {
-    if (xhr.readyState == 4) {
-      // JSON.parse does not evaluate the attacker's scripts.
-      var resp = JSON.parse(xhr.responseText);
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("content").innerHTML = this.responseText;
     }
-  }
-  xhr.send();
-  }
-  
-  window.onload=function loadFirst() {
-    var xhr = new XMLHttpRequest();
-    xhr.open("GET", './website/main.html', true);
-    xhr.onreadystatechange = function() {
-      if (xhr.readyState == 4) {
-        // JSON.parse does not evaluate the attacker's scripts.
-        var resp = JSON.parse(xhr.responseText);
-      }
+  };
+  xhttp.open("GET", page, true);
+  xhttp.send();
+}
+
+window.onload=function loadFirst() {
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("content").innerHTML = this.responseText;
     }
-    xhr.send();
+  };
+  xhttp.open("GET", "main.html", true);
+  xhttp.send();
 }
