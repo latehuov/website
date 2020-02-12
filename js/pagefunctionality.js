407cd4a43a98e1d99548368a6e6666a886553b72
lasse <t8hula00@students.oamk.fi>
2020-02-12 23:58:25 +0200

    pers

--- js/pagefunctionality.js
+++ js/pagefunctionality.js
@@ -1,23 +1,21 @@
 function loadDoc(page) {
-  var xhr = new XMLHttpRequest();
-xhr.open("GET", page, true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    // innerText does not let the attacker inject HTML elements.
-    document.getElementById("content").innerText = xhr.responseText;
-  }
-}
-xhr.send();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("content").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", page, true);
+  xhttp.send();
 }
 
 window.onload=function loadFirst() {
-  var xhr = new XMLHttpRequest();
-xhr.open("GET", "./website/main.html", true);
-xhr.onreadystatechange = function() {
-  if (xhr.readyState == 4) {
-    // innerText does not let the attacker inject HTML elements.
-    document.getElementById("content").innerText = xhr.responseText;
-  }
-}
-xhr.send();
+  var xhttp = new XMLHttpRequest();
+  xhttp.onreadystatechange = function() {
+    if (this.readyState == 4 && this.status == 200) {
+     document.getElementById("content").innerHTML = this.responseText;
+    }
+  };
+  xhttp.open("GET", "./website/main.html", true);
+  xhttp.send();
 }
