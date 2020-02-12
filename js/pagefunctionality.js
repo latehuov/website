57995e86c399b108907fe353c53bfc7eafeeefd0
lasse <t8hula00@students.oamk.fi>
2020-02-12 22:09:22 +0200

    pers

--- js/pagefunctionality.js
+++ js/pagefunctionality.js
@@ -3,9 +3,21 @@ function loadDoc(page) {
   xhr.open("GET", page, true);
   xhr.onreadystatechange = function() {
     if (xhr.readyState == 4) {
-      // innerText does not let the attacker inject HTML elements.
-      document.getElementById("content").innerText = xhr.responseText;
+      // JSON.parse does not evaluate the attacker's scripts.
+      var resp = JSON.parse(xhr.responseText);
     }
   }
   xhr.send();
   }
+  
+  window.onload=function loadFirst() {
+    var xhr = new XMLHttpRequest();
+    xhr.open("GET", './website/main.html', true);
+    xhr.onreadystatechange = function() {
+      if (xhr.readyState == 4) {
+        // JSON.parse does not evaluate the attacker's scripts.
+        var resp = JSON.parse(xhr.responseText);
+      }
+    }
+    xhr.send();
+}
